811e7cea44b62f45a554664f856b9ab0b9b8cb5a
emgord <emgord@gmail.com>
2016-01-25 16:18:33 -0800

    added js to create basic clock

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+
+
+var intervalID = window.setInterval(getDate,1000);
+function getDate(){
+  var time = new Date();
+  var hours = time.getHours()
+    if (hours > 12) {
+      hours -= 12
+      var post = " PM";
+    } else {
+      var post = " AM"
+    }
+  var minutes = time.getMinutes()
+    if (minutes < 10) {
+      minutes = "0" + minutes
+    };
+  var seconds = time.getSeconds()
+    if (seconds < 10) {
+      seconds = "0" + seconds
+    };
+  var clock = document.getElementById("clock");
+  clock.innerHTML = hours+":"+minutes+":"+seconds+post;
+}
